672f49bfa520dab2a9716e193d9210e7f9410e6b
garab <test@gmail.com>
2026-05-15 18:55:55 +0300

    fix railway port

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ def run_flask():
 
 
 async def main():
-    flask_thread = threading.Thread(target=run_flask)
-    flask_thread.daemon = True
+    flask_thread = threading.Thread(target=run_flask, daemon=True)
     flask_thread.start()
 
     await bot_main()
